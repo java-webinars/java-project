791467f177fa6a2498185dc0fae0ba039a68bb3b
java-webinars <asaburov1967@gmail.com>
2018-05-07 19:40:51 +0300

    Lesson_0005

--- student-project/src/SaveStudentOrder.java
+++ student-project/src/SaveStudentOrder.java
@@ -1,6 +1,10 @@
 public class SaveStudentOrder
 {
     public static void main(String[] args) {
-        System.out.println("SaveStudentOrder is running");
+        saveStudentOrder();
+    }
+
+    static void saveStudentOrder() {
+        System.out.println("saveStudentOrder 1");
     }
 }
