66ecce8bd513a3236130b7930c7c1355f85b6eca
java-webinars <asaburov1967@gmail.com>
2018-06-09 04:50:47 +0300

    Lesson_0025

--- student-project/src/edu/javacourse/studentorder/StudentOrderValidator.java
+++ student-project/src/edu/javacourse/studentorder/StudentOrderValidator.java
@@ -11,6 +11,9 @@ import edu.javacourse.studentorder.validator.CityRegisterValidator;
 import edu.javacourse.studentorder.validator.StudentValidator;
 import edu.javacourse.studentorder.validator.WeddingValidator;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StudentOrderValidator
 {
     private CityRegisterValidator cityRegisterVal;
@@ -33,21 +36,22 @@ public class StudentOrderValidator
     }
 
     public void checkAll() {
-        StudentOrder[] soArray = readStudentOrders();
+        List<StudentOrder> soList = readStudentOrders();
 
-        for(StudentOrder so : soArray) {
+        for(StudentOrder so : soList) {
             checkOneOrder(so);
         }
     }
 
-    public StudentOrder[] readStudentOrders() {
-        StudentOrder[] soArray = new StudentOrder[3];
+    public List<StudentOrder> readStudentOrders() {
+        List<StudentOrder> soList = new LinkedList<>();
 
-        for (int c = 0; c < soArray.length; c++) {
-            soArray[c] = SaveStudentOrder.buildStudentOrder(c);
+        for (int c = 0; c < 5; c++) {
+            StudentOrder so = SaveStudentOrder.buildStudentOrder(c);
+            soList.add(so);
         }
 
-        return soArray;
+        return soList;
     }
 
     public void checkOneOrder(StudentOrder so) {
