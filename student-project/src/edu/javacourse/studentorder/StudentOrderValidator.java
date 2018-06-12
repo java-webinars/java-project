bf4b1b8dc0eb5098533239f55d05ec33bf11780f
java-webinars <asaburov1967@gmail.com>
2018-06-12 12:12:41 +0300

    Lesson_0028

--- student-project/src/edu/javacourse/studentorder/StudentOrderValidator.java
+++ student-project/src/edu/javacourse/studentorder/StudentOrderValidator.java
@@ -56,6 +56,7 @@ public class StudentOrderValidator
 
     public void checkOneOrder(StudentOrder so) {
         AnswerCityRegister cityAnswer = checkCityRegister(so);
+
 //        AnswerWedding wedAnswer = checkWedding(so);
 //        AnswerChildren childAnswer = checkChildren(so);
 //        AnswerStudent studentAnswer = checkStudent(so);
