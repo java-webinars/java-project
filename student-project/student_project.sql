73b749724ff7d9521e73425b5a13ce8139961549
Anton Saburov <antonsaburov@Antons-MacBook-Pro.local>
2018-08-05 13:11:11 -0400

    Lesson_0033

--- student-project/student_project.sql
+++ student-project/student_project.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS jc_student_child;
+DROP TABLE IF EXISTS jc_student_order;
 DROP TABLE IF EXISTS jc_passport_office;
 DROP TABLE IF EXISTS jc_register_office;
 DROP TABLE IF EXISTS jc_country_struct;
@@ -33,4 +35,64 @@ CREATE TABLE jc_register_office
     r_office_name varchar(200),
     PRIMARY KEY (r_office_id),
     FOREIGN KEY (r_office_area_id) REFERENCES jc_country_struct(area_id) ON DELETE RESTRICT
+);
+
+
+CREATE TABLE jc_student_order
+(
+    student_order_id SERIAL,
+    h_sur_name varchar(100) not null,
+    h_given_name varchar(100) not null,
+    h_patronymic varchar(100) not null,
+    h_date_of_birth date not null,
+    h_passport_seria varchar(10) not null,
+    h_passport_number varchar(10) not null,
+    h_passport_date date not null,
+    h_passport_office_id integer not null,
+    h_post_index varchar(10),
+    h_street_code integer not null,
+    h_building varchar(10) not null,
+    h_extension varchar(10),
+    h_apartment varchar(10),
+    w_sur_name varchar(100) not null,
+    w_given_name varchar(100) not null,
+    w_patronymic varchar(100) not null,
+    w_date_of_birth date not null,
+    w_passport_seria varchar(10) not null,
+    w_passport_number varchar(10) not null,
+    w_passport_date date not null,
+    w_passport_office_id integer not null,
+    w_post_index varchar(10),
+    w_street_code integer not null,
+    w_building varchar(10) not null,
+    w_extension varchar(10),
+    w_apartment varchar(10),
+    certificate_id varchar(20) not null,
+    register_office_id integer not null,
+    marriage_date date not null,
+    PRIMARY KEY (student_order_id),
+    FOREIGN KEY (h_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (w_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (register_office_id) REFERENCES jc_register_office(r_office_id) ON DELETE RESTRICT
+);
+
+CREATE TABLE jc_student_child
+(
+    student_child_id SERIAL,
+    student_order_id integer not null,
+    c_sur_name varchar(100) not null,
+    c_given_name varchar(100) not null,
+    c_patronymic varchar(100) not null,
+    c_date_of_birth date not null,
+    c_certificate_number varchar(10) not null,
+    c_certificate_date date not null,
+    c_register_office_id integer not null,
+    c_post_index varchar(10),
+    c_street_code integer not null,
+    c_building varchar(10) not null,
+    c_extension varchar(10),
+    c_apartment varchar(10),
+    PRIMARY KEY (student_child_id),
+    FOREIGN KEY (c_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (c_register_office_id) REFERENCES jc_register_office(r_office_id) ON DELETE RESTRICT
 );
