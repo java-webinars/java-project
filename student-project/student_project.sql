5c0359aa209fa6d92994cbf744a525d88692fb54
java-webinars <antonsaburov@Antons-MBP.fios-router.home>
2018-11-01 08:28:02 -0400

    Lesson_0063

--- student-project/student_project.sql
+++ student-project/student_project.sql
@@ -115,3 +115,8 @@ CREATE TABLE jc_student_child
     FOREIGN KEY (c_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
     FOREIGN KEY (c_register_office_id) REFERENCES jc_register_office(r_office_id) ON DELETE RESTRICT
 );
+
+CREATE INDEX idx_student_order_status ON jc_student_order(student_order_status);
+
+CREATE INDEX idx_student_order_id ON jc_student_child(student_order_id);
+
