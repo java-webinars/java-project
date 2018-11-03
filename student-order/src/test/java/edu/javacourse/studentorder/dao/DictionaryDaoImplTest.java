25a5f4a30f386b392be40e360c59a965b0d6e709
java-webinars <antonsaburov@Antons-MBP.fios-router.home>
2018-11-03 19:13:07 -0400

    Lesson_0066

---
+++ student-order/src/test/java/edu/javacourse/studentorder/dao/DictionaryDaoImplTest.java
@@ -0,0 +1,13 @@
+package edu.javacourse.studentorder.dao;
+
+
+import org.junit.Test;
+
+
+public class DictionaryDaoImplTest
+{
+    @Test
+    public void testExample() {
+        System.out.println("TEST");
+    }
+}
